42f571dd628cf80a2d874b6a6d04b93c5966aa59
searchsakib <searchsakib@gmail.com>
2023-08-16 13:10:50 +0600

    project done

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,9 +1,11 @@
 document.getElementById('btn-withdraw').addEventListener('click', () => {
+  // withdraw amount calculation
   const newWithdrawAmount = getInputFieldValueById('withdraw-field');
   const previousWithdrawTotal = getElementValueById('withdraw-total');
   const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
   setTextElementValueById('withdraw-total', newWithdrawTotal);
 
+  // balance amount calculation
   const previousBalanceTotal = getElementValueById('balance-total');
   const balanceTotal = previousBalanceTotal - newWithdrawAmount;
   setTextElementValueById('balance-total', balanceTotal);
